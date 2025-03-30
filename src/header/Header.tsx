670f02185b2d57c1786332c7f325e946d32d214c
Fiodar Siyanka <siyankafiodar@gmail.com>
2025-03-30 12:45:35 +0300

    feat: add i18n (en and ru)

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -3,9 +3,11 @@ import Button from '@/components/button/Button';
 import Image from 'next/image';
 import './header.scss';
 import { useEffect, useState } from 'react';
+import { useLanguage } from '@/hooks/useLanguage';
 
 export default function Header() {
   const [isSticky, setIsSticky] = useState(false);
+  const { currentLang, toggleLanguage, t } = useLanguage();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -20,16 +22,23 @@ export default function Header() {
       <div className="logo">
         <Image src="/next.svg" alt="Logo" width={100} height={100} />
       </div>
-      <Button
-        className="login-button"
-        text="Login"
-        onClick={() => alert('Login clicked')}
-      />
-      <Button
-        className="sign-up-button"
-        text="Sign Up"
-        onClick={() => alert('Sign Up clicked')}
-      />
+      <div className="header-controls">
+        <Button
+          className="language-button"
+          text={currentLang}
+          onClick={toggleLanguage}
+        />
+        <Button
+          className="login-button"
+          text={t('header.login')}
+          onClick={() => alert('Login clicked')}
+        />
+        <Button
+          className="sign-up-button"
+          text={t('header.signup')}
+          onClick={() => alert('Sign Up clicked')}
+        />
+      </div>
     </header>
   );
 }
