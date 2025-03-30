1af476f0cc68d9f8cc300209887c585e97a3227a
Asel <asel.alimbaeva.kk@gmail.com>
2025-03-30 23:06:58 +0300

    feat: add components to parant component

--- src/components/restClient/RestClient.tsx
+++ src/components/restClient/RestClient.tsx
@@ -3,6 +3,9 @@
 import Input from '../ui/inputs/Input';
 import SelectInput from '../ui/inputs/SelectInput';
 import Textarea from '../ui/inputs/Textarea';
+import GeneratedCode from './_components/GeneratedCode';
+import RequestHeaders from './_components/RequestHeaders';
+import RequestSection from './_components/RequestSection';
 import './restClient.scss';
 
 const methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
@@ -21,50 +24,11 @@ const RestClient = () => {
         <Input forInput="path" type="text" customStyle="widthPath" />
         <button className="button">Send</button>
       </div>
-      <div className="request-wrapper">
-        <header className="head-wrapper">
-          <p className="rest-title">Headers:</p>
-          <button className="button">Add Header</button>
-        </header>
-        <div className="path-wrapper">
-          <SelectInput
-            forInput="headers key"
-            type="text"
-            options={methods}
-            customStyle="widthMeth"
-          />
-          <Input forInput="headers value" type="text" customStyle="widthPath" />
-          <button className="button">Remove</button>
-        </div>
-      </div>
-      <div className="request-wrapper">
-        <header className="head-wrapper">
-          <p className="rest-title">Generated request code:</p>
-          <button className="button">Generated</button>
-        </header>
-        <pre className="whitespace-pre-wrap">
-          <code>
-            {`
-               fetch("https://api.example.com/data", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ name: "Alice" })
-                })
-                .then(response => response.json())
-                .then(data => console.log(data))
-                .catch(error => console.error("Error:", error));
-            `}
-          </code>
-        </pre>
-      </div>
-      <div className="request-wrapper">
-        <header className="head-wrapper">
-          <p className="rest-title">Body:</p>
-        </header>
-        <Textarea forInput="body-json"/>
-      </div>
+      <RequestHeaders />
+      <GeneratedCode />
+      <RequestSection title="Body:">
+        <Textarea forInput="body-json" />
+      </RequestSection>
     </section>
   );
 };
