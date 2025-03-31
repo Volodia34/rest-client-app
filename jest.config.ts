267811ee5bc6e8908be2e47954f423c4f8bdd760
Fiodar Siyanka <siyankafiodar@gmail.com>
2025-03-31 18:17:42 +0300

    fix: jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,19 @@
 import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
 
 const config: Config = {
-  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*.d.ts'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 };
 
-export default config;
+export default createJestConfig(config);
