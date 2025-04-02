333ff3ab83526bd3a19adcb6da062560371917f4
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-02 21:22:29 +0300

    feat: add test for SelectInput Component

--- src/components/restClient/restClient.test.tsx
+++ src/components/restClient/restClient.test.tsx
@@ -6,7 +6,6 @@ describe('RestClient Component', () => {
     render(<RestClient />);
 
     expect(screen.getByRole('button', { name: /send/i })).toBeInTheDocument();
-    // expect(screen.getByTestId('select-methods')).toBeInTheDocument();
     expect(await screen.findByTestId('path-wrapper')).toBeInTheDocument();
     expect(screen.getByText(/Generated request code:/i)).toBeInTheDocument();
     expect(screen.getByText(/Body:/i)).toBeInTheDocument();
