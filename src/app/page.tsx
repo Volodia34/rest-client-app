5af13b48f61f0ccbe670344ca222ec149f4d185d
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-03 11:37:19 +0300

    feat: add redux store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import ErrorBoundary from '@/errorsHandlers/ErrorBoundary';
 import Header from '@/components/header/Header';
 import RestClient from '@/components/restClient/RestClient';
 import ResponseBlock from '@/components/restClient/response/ResponseBlock';
+import { Provider } from 'react-redux';
+import { store } from '@/store/store';
 
 export default function Home() {
   return (
     <ErrorBoundary>
-      <Header />
-      <RestClient />
-      <ResponseBlock />
+      <Provider store={store}>
+        <Header />
+        <RestClient />
+        <ResponseBlock />
+      </Provider>
     </ErrorBoundary>
   );
 }
