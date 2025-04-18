f9bcb67f31f80963a2b50c0a97a9c9ee549019dc
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-18 09:59:58 +0300

    feat: add error message for url

--- src/components/restClient/_components/mainURL/HttpMethodURL.tsx
+++ src/components/restClient/_components/mainURL/HttpMethodURL.tsx
@@ -21,6 +21,7 @@ const HttpMethodURL = () => {
   const { method } = useSelector((state: RootState) => state.bodySlice);
   const [filterMethods, setFilterMethods] = useState<string[]>(methods);
   const [render, setRender] = useState(false);
+  const [errorUrl, setErrorUrl] = useState('');
 
   const { variables } = useVariable();
 
@@ -29,23 +30,28 @@ const HttpMethodURL = () => {
   };
 
   const handleUrl = (e: ChangeEvent<HTMLInputElement>) => {
+    if (errorUrl) setErrorUrl('')
     let rawUrl = e.target.value;
     if (isVariables(rawUrl)) {
       rawUrl = replaceVariables(rawUrl, variables);
     }
-    dispatch(setUrlValueInput(rawUrl));
-    const url = new URL(rawUrl);
+    try {
+      const url = new URL(rawUrl);
+      dispatch(setUrlValueInput(rawUrl));
 
-    dispatch(setBaseUrl(`${url.protocol}//${url.host}`));
-    dispatch(setEndpoint(url.pathname));
+      dispatch(setBaseUrl(`${url.protocol}//${url.host}`));
+      dispatch(setEndpoint(url.pathname));
 
-    const params = new URLSearchParams(url.search);
+      const params = new URLSearchParams(url.search);
 
-    dispatch(
-      setParamsAndEncode({
-        params: params.toString(),
-      })
-    );
+      dispatch(
+        setParamsAndEncode({
+          params: params.toString(),
+        })
+      );
+    } catch {
+      setErrorUrl('error in the url')
+    }
   };
 
   const handleChangeMethod = (e: ChangeEvent<HTMLInputElement>): string[] => {
@@ -63,26 +69,29 @@ const HttpMethodURL = () => {
   }, [method]);
 
   return (
-    <div key={`${render}`} className="path-wrapper" data-testid="path-wrapper">
-      <SelectInput
-        data-test="select-methods"
-        forInput="methods"
-        type="text"
-        options={filterMethods}
-        customStyle="widthMeth"
-        value={method}
-        onChange={handleChangeMethod}
-        onSelect={handleSelectMethod}
-      />
-      <Input
-        forInput="path"
-        type="text"
-        value={urlValueInput}
-        customStyle="widthPath"
-        onChange={handleUrl}
-      />
-      <Button className="button" text={'Send'} onClick={() => {}} />
-    </div>
+    <>
+      <div key={`${render}`} className="path-wrapper" data-testid="path-wrapper">
+        <SelectInput
+          data-test="select-methods"
+          forInput="methods"
+          type="text"
+          options={filterMethods}
+          customStyle="widthMeth"
+          value={method}
+          onChange={handleChangeMethod}
+          onSelect={handleSelectMethod}
+        />
+        <Input
+          forInput="path"
+          type="text"
+          value={urlValueInput}
+          customStyle="widthPath"
+          onChange={handleUrl}
+        />
+        <Button className="button" text={'Send'} onClick={() => {}} />
+      </div>
+      {errorUrl && <p className="warning-messages">{errorUrl}</p>}
+    </>
   );
 };
 
