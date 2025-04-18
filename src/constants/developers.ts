d5ba3bb48437903846bdc861a2f3c0d47573de13
xFedSix <103776412+xFedSix@users.noreply.github.com>
2025-04-18 20:04:27 +0300

    Revert "feature/rest proxy"

--- src/constants/developers.ts
+++
@@ -1,5 +0,0 @@
-export const developers = [
-  { name: 'Kisil Volodia', url: 'https://github.com/Volodia34' },
-  { name: 'Alymbaeva Asel ', url: 'https://github.com/alimbaeva/alimbaeva' },
-  { name: 'Siyanka Fiodar', url: 'https://github.com/xFedSix' },
-];
